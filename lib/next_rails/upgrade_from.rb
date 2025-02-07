043411cc0724a303f8203c6065b430d0f401e3d6
Juan Vasquez <juan@ombulabs.com>
2025-02-07 14:08:38 -0600

    Small refactor
    I hope you agree with

--- lib/next_rails/upgrade_from.rb
+++ lib/next_rails/upgrade_from.rb
@@ -1,12 +1,11 @@
 require_relative "http_client"
 
 class NextRails::UpgradeFrom
-
   URL = ENV.fetch("UPGRADE_FROM_URL") { "https://roadrunner-staging-5ff1a4e7a439.herokuapp.com/api/v1/next_versions" }
 
   def initialize(current_version)
     @current_version = current_version
-    @next_versions_res = NextRails::HttpClient.get(URL, {current_rails_version: @current_version})
+    @response = NextRails::HttpClient.get(URL, {current_rails_version: @current_version})
   end
 
   def self.current_version(current_version)
@@ -14,26 +13,26 @@ class NextRails::UpgradeFrom
   end
 
   def print_versions
-    return @next_versions_res['detail'] if @next_versions_res.fetch('detail', false)
+    return @response['detail'] if @response.fetch('detail', false)
+
     check_rails_version + check_ruby_version
   end
 
   private
 
   def ruby_version
-    @next_versions_res['required_ruby']
+    @response['required_ruby']
   end
 
   def check_rails_version
-    if @next_versions_res["next_rails"].nil?
-      "You are on the most current Rails version."
-    else
-      "The latest patch for Rails #{@current_version} is #{@next_versions_res['current_latest_patch']}.\nThe next Rails target jump should be #{@next_versions_res["next_rails"]}.\n"
-    end
+    return "You are on the most current Rails version." if @response['next_rails'].nil?
+
+    "The latest patch for Rails #{@current_version} is #{@response['current_latest_patch']}.\nThe next Rails target jump should be #{@response['next_rails']}.\n"
   end
 
   def check_ruby_version
     return "" if ruby_version.empty?
+
     "The recommended Ruby version#{'s' if ruby_version.length > 1} for this jump is #{ruby_version.join(', ')}."
   end
 end
