a4dd55ba2d836e1bbedea94023cf80fd5aa08032
Juan Vasquez <juan@ombulabs.com>
2025-02-11 14:02:38 -0600

    Update test according to latest changes

--- spec/next_rails/upgrade_from_spec.rb
+++ spec/next_rails/upgrade_from_spec.rb
@@ -1,9 +1,7 @@
 require "spec_helper"
-require "next_rails/upgrade_from"
 
 RSpec.describe NextRails::UpgradeFrom do
   let(:current_version) { "6.0.0" }
-  let(:mock_url) { "https://wwww.example.com/api/v1/next_versions" }
   let(:response_body) do
     {
       current_latest_patch: "6.0.3.7",
@@ -12,16 +10,10 @@ RSpec.describe NextRails::UpgradeFrom do
     }.to_json
   end
 
-  before do
-    stub_const("NextRails::UpgradeFrom::URL", mock_url)
-    stub_request(:get, mock_url)
-      .with(query: { current_rails_version: current_version })
-      .to_return(status: 200, body: response_body, headers: { "Content-Type" => "application/json" })
-  end
-
-  describe ".current_version" do
+  describe ".report" do
     it "returns the next versions information" do
-      result = described_class.current_version(current_version)
+      response = JSON.parse(response_body)
+      result = described_class.report(current_version, response)
       expect(result).to include("The latest patch for Rails 6.0.0 is 6.0.3.7.")
       expect(result).to include("The next Rails target upgrade should be 6.1.0.")
       expect(result).to include("The recommended Ruby version for the upgrade is 2.7.2.")
@@ -39,7 +31,8 @@ RSpec.describe NextRails::UpgradeFrom do
       end
 
       it "returns a message indicating the current Rails version is the latest" do
-        result = described_class.new(current_version).print_versions
+        response = JSON.parse(response_body)
+        result = described_class.new(current_version, response).print_versions
         expect(result).to include("You are on the most current Rails version.")
       end
     end
@@ -55,9 +48,10 @@ RSpec.describe NextRails::UpgradeFrom do
       end
 
       it "suggests upgrading to the latest patch version first and then the next Rails version" do
-        result = described_class.new(current_version).print_versions
+        response = JSON.parse(response_body)
+        result = described_class.new(current_version, response).print_versions
         expect(result).to include("The latest patch for Rails 6.0.0 is 6.0.3.7.")
-        expect(result).to include("Before going to the next Rails version, it is strongly recommended upgrading to the latest patch (v6.0.3.7).")
+        expect(result).to include("It is strongly recommended to upgrade to the latest patch before upgrading to the next Rails version.")
         expect(result).to include("The next Rails target upgrade should be 6.1.0.")
       end
     end
@@ -73,7 +67,8 @@ RSpec.describe NextRails::UpgradeFrom do
       end
 
       it "suggests upgrading to the next Rails version" do
-        result = described_class.new(current_version).print_versions
+        response = JSON.parse(response_body)
+        result = described_class.new(current_version, response).print_versions
         expect(result).to include("Your application is running the latest patch version 6.0.3.7, which is great!")
         expect(result).to include("The next Rails target upgrade should be 6.1.0.")
       end
@@ -85,7 +80,8 @@ RSpec.describe NextRails::UpgradeFrom do
       end
 
       it "returns the detail message" do
-        result = described_class.new(current_version).print_versions
+        response = JSON.parse(response_body)
+        result = described_class.new(current_version, response).print_versions
         expect(result).to eq("Some detail message")
       end
     end
